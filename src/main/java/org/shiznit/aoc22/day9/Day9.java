b0052a155dbc13a39849e3b6d7cb47ed1b56280c
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2022-12-12 12:24:29 -0500

    Finished Day 9 Part 2

--- src/main/java/org/shiznit/aoc22/day9/Day9.java
+++ src/main/java/org/shiznit/aoc22/day9/Day9.java
@@ -1,78 +1,141 @@
 package org.shiznit.aoc22.day9;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day9 {
+
+    List<Motion> motions;
 
     public Day9() throws IOException {
         System.out.println("Running Day 9 - Part 1");
 
         List<String> lines = Files.readAllLines(Path.of("src/main/java/org/shiznit/aoc22/day9/input.txt"));
 
-        Knot head = new Knot();
-        Knot tail = new Knot();
+        motions = lines.stream()
+                .map(Motion::parse)
+                .collect(Collectors.toList());
 
-        Table<Integer, Integer, Integer> tailVisits = HashBasedTable.create();
+        List<Knot> knots = IntStream.range(0, 2).mapToObj(i -> new Knot()).collect(Collectors.toList());
 
-        for (String line : lines) {
-            String[] parts = line.split(" ");
+        System.out.println("Num Visited Positions = " + simulate(knots));
+
+        System.out.println("Running Day 9 - Part 2");
+
+        knots = IntStream.range(0, 10).mapToObj(i -> new Knot()).collect(Collectors.toList());
+
+        System.out.println("Num Visited Positions = " + simulate(knots));
+    }
+
+    int simulate(List<Knot> knots) {
+        Knot head = knots.get(0);
+
+        Set<Pair<Integer, Integer>> tailVisits = new HashSet<>();
+        tailVisits.add(Pair.of(0,0));
+
+        for (Motion motion : motions) {
             int hdx = 0, hdy = 0;
-            if (parts[0].equals("R")) {
-                hdx = 1;
-            } else if (parts[0].equals("L")) {
-                hdx = -1;
-            } else if (parts[0].equals("U")) {
-                hdy = 1;
-            } else if (parts[0].equals("D")) {
-                hdy = -1;
+            switch (motion.dir) {
+                case RIGHT:
+                    hdx = 1;
+                    break;
+                case LEFT:
+                    hdx = -1;
+                    break;
+                case UP:
+                    hdy = 1;
+                    break;
+                case DOWN:
+                    hdy = -1;
+                    break;
             }
 
-            int distance = Integer.parseInt(parts[1]);
-
-            for (int i = 0; i < distance; ++i) {
-
+            // Move each knot in succession.
+            for (int i = 0; i < motion.distance; ++i) {
                 // Move head.
                 head.x += hdx;
                 head.y += hdy;
 
-                // Move tail.
-                int tdx = tail.x - head.x;
-                int tdy = tail.y - head.y;
+                Knot prev = head;
 
-                // Only move the tail if it's not "touching" the head.
-                if (!(Math.abs(tdx) <= 1 && Math.abs(tdy) <= 1)) {
-                    if (tdx != 0 && tdy != 0) {
-                        // Move the tail one space diagonally toward head.
-                        tail.x = (tdx > 0) ? tail.x - 1 : tail.x + 1;
-                        tail.y = (tdy > 0) ? tail.y - 1 : tail.y + 1;
-                    } else if (tdx != 0) {
-                        // Move the tail one space horizontally toward head.
-                        tail.x = (tdx > 0) ? tail.x - 1 : tail.x + 1;
-                    } else if (tdy != 0) {
-                        // Move the tail one space vertically toward head.
-                        tail.y = (tdy > 0) ? tail.y - 1 : tail.y + 1;
+                for (int j = 1; j < knots.size(); ++ j) {
+                    Knot knot = knots.get(j);
+
+                    // Move the knot.
+                    int tdx = knot.x - prev.x;
+                    int tdy = knot.y - prev.y;
+
+                    // Only move the knot if it's not "touching" the one before it.
+                    if (!(Math.abs(tdx) <= 1 && Math.abs(tdy) <= 1)) {
+                        if (tdx != 0 && tdy != 0) {
+                            // Move the knot one space diagonally toward the one before it.
+                            knot.x = (tdx > 0) ? knot.x - 1 : knot.x + 1;
+                            knot.y = (tdy > 0) ? knot.y - 1 : knot.y + 1;
+                        } else if (tdx != 0) {
+                            // Move the knot one space horizontally toward the one before it.
+                            knot.x = (tdx > 0) ? knot.x - 1 : knot.x + 1;
+                        } else if (tdy != 0) {
+                            // Move the knot one space vertically toward the one before it.
+                            knot.y = (tdy > 0) ? knot.y - 1 : knot.y + 1;
+                        }
                     }
+
+                    prev = knot;
                 }
 
                 // Mark the tail's visit.
-                Integer visits = tailVisits.get(tail.x, tail.y);
-                visits = (visits == null) ? 1 : visits + 1;
-                tailVisits.put(tail.x, tail.y, visits);
+                tailVisits.add(Pair.of(prev.x, prev.y));
             }
         }
 
-        // The number of cells in the table is the answer for part 1.
-        System.out.println("Num Visited Positions = " + tailVisits.cellSet().size());
+        return tailVisits.size();
     }
 
     static class Knot {
         int x;
         int y;
+    }
+
+    enum Direction {
+        RIGHT("R"),
+        LEFT("L"),
+        UP("U"),
+        DOWN("D");
+
+        final String input;
+
+        Direction(String input) {
+            this.input = input;
+        }
+
+        public static Direction parse(String input) {
+            return Arrays.stream(Direction.values())
+                    .filter(d -> d.input.equals(input))
+                    .findFirst().orElseThrow();
+        }
+    }
+
+    static class Motion {
+        final Direction dir;
+        final int distance;
+
+        public static Motion parse(String line) {
+            String[] parts = line.split(" ");
+            return new Motion(Direction.parse(parts[0]), Integer.parseInt(parts[1]));
+        }
+
+        Motion(Direction dir, int distance) {
+            this.dir = dir;
+            this.distance = distance;
+        }
     }
 }
