9869e008a0a61c6913dba93df0fa217e7a0786c4
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2022-12-12 22:38:42 -0500

    Changed main to use reflection to run each day instead of requiring daily edits

--- src/main/java/org/shiznit/AdventOfCode2022.java
+++ src/main/java/org/shiznit/AdventOfCode2022.java
@@ -1,31 +1,18 @@
 package org.shiznit;
 
-import org.shiznit.aoc22.day1.Day1;
-import org.shiznit.aoc22.day10.Day10;
-import org.shiznit.aoc22.day2.Day2;
-import org.shiznit.aoc22.day3.Day3;
-import org.shiznit.aoc22.day4.Day4;
-import org.shiznit.aoc22.day5.Day5;
-import org.shiznit.aoc22.day6.Day6;
-import org.shiznit.aoc22.day7.Day7;
-import org.shiznit.aoc22.day8.Day8;
-import org.shiznit.aoc22.day9.Day9;
-
-import java.io.IOException;
-
 public class AdventOfCode2022 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("\n🎅🎅🎅🎅🎅 ADVENT OF CODE 2022 🎅🎅🎅🎅🎅\n");
 
-        new Day1();
-        new Day2();
-        new Day3();
-        new Day4();
-        new Day5();
-        new Day6();
-        new Day7();
-        new Day8();
-        new Day9();
-        new Day10();
+        for (int i = 1; i <= 25; ++i) {
+            try {
+                Class<?> day = Class.forName("org.shiznit.aoc22.day" + i + ".Day" + i);
+                long time = System.currentTimeMillis();
+                day.getConstructor().newInstance();
+                System.out.printf("Elapsed = %d ms\n\n", System.currentTimeMillis() - time);
+            } catch (ReflectiveOperationException e) {
+                // probably doesn't exist yet.
+            }
+        }
     }
 }
